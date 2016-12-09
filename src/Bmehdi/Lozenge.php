c2516ed9e05f217e050d4e65460d62d5e7b74561
Nimothenicefish <nimo.moulin@gmail.com>
2016-12-09 16:48:10 +0100

    Test Lozenge is OK

--- src/Bmehdi/Lozenge.php
+++ src/Bmehdi/Lozenge.php
@@ -38,13 +38,68 @@ class Lozenge
      * o     o
      *  o   o 
      *   o o  
-     *    o   
+     *    o
+     *     *
+     *     o
+     *    o o
+     *   o   o
+     *  o     o
+     * o       o
+     *  o     o
+     *   o   o
+     *    o o
+     *     o
      *
      * Etc.
      */
     public function __toString()
     {
-        $lozenge = '';
+        if (0 == $this->size) {
+            return '';
+        }
+
+        $outside = str_repeat(" ", $this->size);
+        $lozenge = <<<L
+{$outside}o{$outside}
+L;
+
+        $separatorOutside = $this->size - 1;
+        $separatorInside = 1;
+        for ($i = 1; $i < $this->size; $i++) {
+            $outside = str_repeat(" ", $separatorOutside);
+            $inside = str_repeat(" ", $separatorInside);
+            $lozenge .= <<<L
+
+{$outside}o{$inside}o{$outside}
+L;
+            $separatorOutside--;
+            $separatorInside = $separatorInside + 2;
+        }
+
+            $inside = str_repeat(" ", $separatorInside);
+            $lozenge .= <<<L
+
+o{$inside}o
+L;
+
+        $separatorOutside = 1;
+        $separatorInside = $separatorInside - 2;
+        for ($i = $this->size; $i > 1; $i--) {
+            $outside = str_repeat(" ", $separatorOutside);
+            $inside = str_repeat(" ", $separatorInside);
+            $lozenge .= <<<L
+
+{$outside}o{$inside}o{$outside}
+L;
+            $separatorOutside++;
+            $separatorInside = $separatorInside - 2;
+        }
+
+        $outside = str_repeat(" ", $this->size);
+        $lozenge .= <<<L
+
+{$outside}o{$outside}
+L;
 
         // todo : update $lozenge
 
